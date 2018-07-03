c11c81eee10eabb1df8b53e9868ac06751c9947d
ShilpaSejwal <40684182+ShilpaSejwal@users.noreply.github.com>
2018-07-03 12:41:39 +0530

    Add files via upload

---
+++ second,py.py
@@ -0,0 +1,35 @@
+#1
+
+print("first assignment")
+print(3)
+
+
+
+#2
+
+a = ("good ")
+b = ("morning")
+c = a+b
+print(c)
+
+#3
+x,y,z = 10,20,30
+print(x,y,z)
+
+#4
+
+print("lets get started")
+
+# 5
+
+s = "acadview"
+course = "python"
+fees = 5000
+print("i am doing course from %s and course name is %s and the fees is %d" %(s,course,fees)) 
+
+
+#6
+
+name = "tony stark"
+salary = 1000000
+print('%s %d' %(name,salary))
