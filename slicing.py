86c2d0a63c2117a46f02c1face18251b82ef05d1
ShilpaSejwal <40684182+ShilpaSejwal@users.noreply.github.com>
2018-08-03 21:03:20 +0530

    Add files via upload

---
+++ slicing.py
@@ -0,0 +1,16 @@
+#1
+#front
+
+x = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+print("slicing from front",x[7:12])
+
+#back
+print("slicing from end",x[11:6:-1])
+
+#2
+
+print("even" ,x[1::2])
+
+#3
+
+print("every 4th number:",x[::4])
